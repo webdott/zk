a56a76e981e32a0f278002f62fbb84a2f66d0676
caspero-62 <nwaforuchechukwu2007@gmail.com>
2025-03-28 21:31:36 +0100

    chore: init merkle prove function

--- fri/src/merkle.rs
+++ fri/src/merkle.rs
@@ -1,14 +1,17 @@
 use ark_ff::{BigInteger, PrimeField};
+use std::marker::PhantomData;
+
 use fiat_shamir::transcript::{GenericHashFunctionTrait, GenericTranscript};
 
 pub struct MerkleTree<T: PrimeField, F: GenericHashFunctionTrait> {
+    _marker: PhantomData<F>,
     hash_layers: Vec<Vec<T>>,
 }
 
 pub struct MerkleProof {}
 
 impl<T: PrimeField, F: GenericHashFunctionTrait> MerkleTree<T, F> {
-    pub fn getProof() -> MerkleProof {
+    pub fn get_proof() -> MerkleProof {
         todo!()
     }
 
@@ -22,24 +25,71 @@ impl<T: PrimeField, F: GenericHashFunctionTrait> MerkleTree<T, F> {
             current_layer.append(&mut vec![T::one(); rem_length]);
         }
 
+        // hash initial layer
+        current_layer = current_layer
+            .iter()
+            .map(|val| transcript.get_hash(&val.into_bigint().to_bytes_le()))
+            .collect::<Vec<_>>();
+
         while current_layer.len() > 1 {
-            let hashed_layer = current_layer
-                .iter()
-                .map(|val| {
-                    let hashed_val = transcript.get_hash(&val.into_bigint().to_bytes_le());
-                    hashed_val
-                })
-                .collect::<Vec<_>>();
+            let mut next_layer = Vec::with_capacity(current_layer.len() / 2);
 
-            self.hash_layers.push(hashed_layer.to_vec());
+            for mut i in 0..current_layer.len() / 2 {
+                next_layer.push(
+                    transcript.get_hash(
+                        &(current_layer[i] * current_layer[i + 1])
+                            .into_bigint()
+                            .to_bytes_le(),
+                    ),
+                );
 
-            current_layer = hashed_layer;
+                i += 1;
+            }
+
+            self.hash_layers.push(next_layer.to_vec());
+
+            current_layer = next_layer;
         }
 
-        todo!()
+        println!("{}", self.hash_layers);
     }
 
-    pub fn verifyProof() -> bool {
+    pub fn verify_proof(&mut self, root_hash: &T, proof: MerkleProof) -> bool {
         todo!()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    use ark_bn254::Fq;
+    use sha3::digest::core_api::CoreWrapper;
+    use sha3::{Digest, Keccak256, Keccak256Core};
+
+    use fiat_shamir::transcript::GenericTranscript;
+
+    #[test]
+    fn test_merkle() {
+        let mut merkle_tree: MerkleTree<Fq, CoreWrapper<Keccak256Core>> = MerkleTree {
+            _marker: PhantomData,
+            hash_layers: vec![],
+        };
+
+        merkle_tree.prove(
+            &[
+                Fq::from(1),
+                Fq::from(2),
+                Fq::from(3),
+                Fq::from(4),
+                Fq::from(5),
+                Fq::from(6),
+                Fq::from(7),
+                Fq::from(8),
+            ],
+            &mut GenericTranscript::new(Keccak256::new()),
+        );
+
+        assert!(true)
     }
 }
